d1cb7747c9088719c5b959c93b39aa61655f1f99
androidFrameworkDev <6573890+androidFrameworkDev@users.noreply.github.com>
2018-12-24 20:22:32 +0530

    create BoundServiceIBinder project

---
+++ services/BoundServiceIBinder/app/src/main/java/services/practice/BindingActivity.java
@@ -0,0 +1,67 @@
+package services.practice;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class BindingActivity extends AppCompatActivity {
+    BoundService boundService;
+    boolean isBound = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_binding);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Bind to BoundService
+        Intent intent = new Intent(this, BoundService.class);
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unbindService(serviceConnection);
+        isBound = false;
+    }
+
+    /** Called when a button is clicked (the button in the layout file attaches to
+     * this method with the android:onClick attribute) */
+    public void onButtonClick(View v) {
+        if (isBound) {
+            // Call a method from the BoundService.
+            // However, if this call were something that might hang, then this request should
+            // occur in a separate thread to avoid slowing down the activity performance.
+            int num = boundService.getRandomNumber();
+            Toast.makeText(this, "number: " + num, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder iBinder) {
+            // We've bound to BoundService, cast the IBinder and get BoundService instance
+            BoundService.MyBinder binder = (BoundService.MyBinder) iBinder;
+            boundService = binder.getService();
+            isBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            isBound = false;
+        }
+    };
+}
---
+++ services/BoundServiceIBinder/app/src/main/java/services/practice/BoundService.java
@@ -0,0 +1,46 @@
+package services.practice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+import java.util.Random;
+
+public class BoundService extends Service {
+
+    // Binder given to clients
+    private final IBinder iBinder = new MyBinder();
+
+    /* system caches the IBinder service communication channel. In other words,
+     * the system calls the service's onBind() method to generate the IBinder only when the first client binds.
+     * The system then delivers that same IBinder to all additional clients that bind to that same service,
+     * without calling onBind() again. */
+
+    // Random number generator
+    private final Random random = new Random();
+
+    /**
+     * Class used for the client Binder.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with IPC.
+     */
+    public class MyBinder extends Binder {
+        // Return this instance of BoundService so clients can call public methods
+        BoundService getService() {
+            return BoundService.this;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+
+        //this iBinder is passed to ServiceConnection's onServiceConnected() method
+        // in the client binding to this service
+        return iBinder;
+    }
+
+    /** method for clients */
+    public int getRandomNumber() {
+        return random.nextInt(100);
+    }
+}
